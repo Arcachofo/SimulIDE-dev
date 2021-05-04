9cc5fbe5e59eb8c1853053ff6a30a5c63b3a6f73
arcachofo <arcachofo@simulide.com>
2021-05-04 20:34:42 +0100

    McuInterface files missing.

---
+++ src/mcusim/mcuinterface.cpp
@@ -0,0 +1,148 @@
+/***************************************************************************
+ *   Copyright (C) 2021 by santiago González                               *
+ *   santigoro@gmail.com                                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>.  *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "mcuinterface.h"
+#include "utils.h"
+
+McuInterface::McuInterface( QString id )
+            : eElement( id )
+            , m_ramTable( NULL, this )
+{
+    m_ramSize   = 0;
+    m_flashSize = 0;
+    m_romSize   = 0;
+    m_wordSize  = 2;
+
+    m_ramTable.hide();
+}
+
+McuInterface::~McuInterface()
+{
+}
+
+QVector<int>* McuInterface::eeprom()
+{
+    for( uint32_t i=0; i<m_romSize; ++i ) m_eeprom[i] = getRomValue( i );
+    return &m_eeprom;
+}
+
+void McuInterface::setEeprom( QVector<int>* eep )
+{
+    int size = m_romSize;
+    if( eep->size() < size ) size = eep->size();
+
+    for( int i=0; i<size; ++i ) setRomValue( i, eep->at(i) );
+}
+
+void McuInterface::updateRamValue( QString name )
+{
+    //if( !m_loadStatus ) return;
+
+    name = name.toUpper();
+    QString type = "u8";
+    if( m_typeTable.contains( name )) type = m_typeTable[ name ];
+    //else return;
+
+    QByteArray ba;
+    ba.resize(4);
+    int address = getRegAddress( name );
+    if( address < 0 ) return;
+
+    int bits = 8;
+
+    if( type.contains( "32" ) )    // 4 bytes
+    {
+        bits = 32;
+        ba[0] = getRamValue( address );
+        ba[1] = getRamValue( address+1 );
+        ba[2] = getRamValue( address+2 );
+        ba[3] = getRamValue( address+3 );
+    }
+    else if( type.contains( "16" ) )  // 2 bytes
+    {
+        bits = 16;
+        ba[0] = getRamValue( address );
+        ba[1] = getRamValue( address+1 );
+        ba[2] = 0;
+        ba[3] = 0;
+    }
+    else                                  // 1 byte
+    {
+        ba[0] = getRamValue( address );
+        ba[1] = 0;
+        ba[2] = 0;
+        ba[3] = 0;
+    }
+    if( type.contains( "f" ) )                          // float, double
+    {
+        float value = 0;
+        memcpy(&value, ba, 4);
+        m_ramTable.setItemValue( 1, value  );
+    }
+    else                                              // char, int, long
+    {
+        int32_t value = 0;
+
+        if( type.contains( "u" ) )
+        {
+            uint32_t val = 0;
+            memcpy(&val, ba, 4);
+            value = val;
+        }
+        else
+        {
+            if( bits == 32 )
+            {
+                int32_t val = 0;
+                memcpy(&val, ba, 4);
+                value = val;
+            }
+            else if( bits == 16 )
+            {
+                int16_t val = 0;
+                memcpy(&val, ba, 2);
+                value = val;
+            }
+            else
+            {
+                int8_t val = 0;
+                memcpy(&val, ba, 1);
+                value = val;
+            }
+        }
+        m_ramTable.setItemValue( 2, value  );
+
+        if     ( type.contains( "8" ) ) m_ramTable.setItemValue( 3, decToBase(value, 2, 8)  );
+        else if( type.contains( "string" ) )
+        {
+            QString strVal = "";
+            for( int i=address; i<=address+value; i++ )
+            {
+                QString str = "";
+                const QChar cha = getRamValue( i );
+                str.setRawData( &cha, 1 );
+
+                strVal += str; //QByteArray::fromHex( getRamValue( i ) );
+            }
+            //qDebug() << "string" << name << value << strVal;
+            m_ramTable.setItemValue( 3, strVal  );
+        }
+    }
+    m_ramTable.setItemValue( 1, type  );
+}
---
+++ src/mcusim/mcuinterface.h
@@ -0,0 +1,80 @@
+/***************************************************************************
+ *   Copyright (C) 2021 by santiago González                               *
+ *   santigoro@gmail.com                                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, see <http://www.gnu.org/licenses/>.  *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef MCUINTERFACE_H
+#define MCUINTERFACE_H
+
+#include "e-element.h"
+#include "ramtable.h"
+
+class MAINMODULE_EXPORT McuInterface : public eElement
+{
+
+    public:
+        McuInterface( QString id );
+        ~McuInterface();
+
+        virtual uint8_t getRamValue( int address )=0;
+        virtual void    setRamValue( int address, uint8_t value )=0;
+        virtual uint16_t getFlashValue( int address )=0;
+        virtual void     setFlashValue( int address, uint16_t value )=0;
+        virtual uint8_t getRomValue( int address )=0;
+        virtual void    setRomValue( int address, uint8_t value )=0;
+
+        virtual uint16_t getRegAddress( QString name ) =0;
+
+        virtual int status()=0;
+        virtual int pc()=0;
+
+        virtual QStringList getRegList() { return m_regList; }
+
+        virtual QVector<int>* eeprom();
+        virtual void setEeprom( QVector<int>* eep );
+
+        uint32_t ramSize()  { return m_ramSize; }
+        uint32_t flashSize(){ return m_flashSize; }
+        uint32_t romSize()  { return m_romSize; }
+        uint32_t wordSize() { return m_wordSize; }
+
+        virtual void updateRamValue( QString name );
+
+        QString getFileName() { return m_firmware; }
+        virtual RamTable* getRamTable() { return &m_ramTable; }
+
+    protected:
+
+        QString m_firmware;     // firmware file loaded
+
+        QStringList m_regList;  // List of Register names
+
+        QVector<int> m_eeprom;
+
+        QHash<QString, QString> m_typeTable;
+
+        uint32_t m_ramSize;
+        uint32_t m_flashSize;
+        uint32_t m_romSize;
+        uint8_t  m_wordSize; // Size of Program memory word in bytes
+
+        RamTable m_ramTable;
+};
+
+
+#endif
+
