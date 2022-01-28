22f4f0d87ab1b8a2c21e207485794afa0beb8d53
arcachofo <arcachofo@simulide.com>
2022-01-28 07:39:45 +0000

    PICs: Add Pullups to xml files.

--- src/mcusim/cores/pic/picadc.cpp
+++ src/mcusim/cores/pic/picadc.cpp
@@ -27,9 +27,9 @@ PicAdc* PicAdc::createAdc( eMcu* mcu, QString name, int type )
 {
     switch( type ){
         case 00: return new PicAdc00( mcu, name ); break;
-        case 20: return new PicAdc20( mcu, name ); break;
         case 10: return new PicAdc10( mcu, name ); break;
         case 11: return new PicAdc11( mcu, name ); break;
+        case 20: return new PicAdc20( mcu, name ); break;
         default: return NULL;
 }   }
 
