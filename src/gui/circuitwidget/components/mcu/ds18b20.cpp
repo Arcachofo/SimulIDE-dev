7bbf343cb4b2e28ed0cf0426504a721e0e5855c3
arcachofo <arcachofo@simulide.com>
2022-05-03 00:30:25 +0100

    Ds18b20: Basic implementation kindof working.

--- src/gui/circuitwidget/components/mcu/ds18b20.cpp
+++ src/gui/circuitwidget/components/mcu/ds18b20.cpp
@@ -1,11 +1,11 @@
 /***************************************************************************
- *   Copyright (C) 2018 by santiago González                               *
+ *   Copyright( C) 2018 by santiago González                               *
  *   santigoro@gmail.com                                                   *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
  *   the Free Software Foundation; either version 3 of the License, or     *
- *   (at your option) any later version.                                   *
+ *  ( at your option) any later version.                                   *
  *                                                                         *
  *   This program is distributed in the hope that it will be useful,       *
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
@@ -31,6 +31,10 @@
 
 #include "doubleprop.h"
 
+// Internal submethods/functions to actually process the job
+uint8_t crc8( uint8_t *addr, uint8_t len);
+void generateROM(uint8_t familyCode, uint8_t *ROM);
+
 Component* Ds18b20::construct( QObject* parent, QString type, QString id )
 { return new Ds18b20( parent, type, id ); }
 
@@ -44,17 +48,66 @@ LibraryItem* Ds18b20::libraryItem()
         Ds18b20::construct );
 }
 
+char chex[]={
+    '0','1','2','3','4','5','6','7','8','9',
+    'A','B','C','D','E','F'
+};
+
+void QDeb_HEX( std::string str, uint8_t* data, int len )
+{
+  QDebug q = qDebug().nospace().noquote();
+
+  q << str.c_str() << ' ';
+
+  for(int i=0; i<len; i++)
+  {
+    uint8_t d = data[i];
+
+    q << chex[d>>4];
+    q << chex[d & 0x0F];
+    q << ' ';
+  }
+}
+
+void QDeb_HEX( std::string str, std::vector<u_int8_t> data )
+{
+  QDebug q = qDebug().nospace().noquote();
+
+  q << str.c_str() << ' ';
+
+  for( uint i=0; i<data.size(); i++)
+  {
+    uint8_t d = data[i];
+
+    q << chex[d>>4];
+    q << chex[d & 0x0F];
+    q << ' ';
+  }
+}
+
 Ds18b20::Ds18b20( QObject* parent, QString type, QString id )
        : Component( parent, type, id )
        , eElement( id )
-{
-    m_area = QRect(-28,-16, 56, 32 );
+{    
+    m_area = QRect(-28,-16, 70, 32 );
 
-    m_temp = 22;
+    m_state = W1_IDLE;
+    m_parPower = false;
+    m_resolution = 12; // Should check what is default 9-12, should be writable in EEPROM
     m_tempInc = 0.5;
+    setTemp( 22 );
+
+    // This actually should be generated and saved with sim file
+    // once component is dropped. It is unique ID to keep
+    generateROM( 0x28, m_ROM );
+
+    m_TH_reg  = 0x4B; /// TODO, check default value
+    m_TL_reg  = 0x46; /// TODO, check default value
+    m_CFG_reg = 0x7F; /// TODO, check default value
 
     m_pin.resize(1);
     m_pin[0] = m_inpin = new IoPin( 180, QPoint(-36, 8), id+"-inPin", 0, this, openCo );
+
     m_inpin->setOutHighV( 5 );
     m_inpin->setLabelColor( QColor( 250, 250, 200 ) );
     m_inpin->setLabelText("DQ");
@@ -80,14 +133,16 @@ Ds18b20::Ds18b20( QObject* parent, QString type, QString id )
     proxy->setPos( QPoint( 9, 4 ) );
 
     connect( u_button, SIGNAL( pressed()),
-             this,     SLOT  ( upbuttonclicked()) );
+             this,     SLOT (  upbuttonclicked()) );
 
     connect( d_button, SIGNAL( pressed()),
-             this,     SLOT  ( downbuttonclicked()) );
+             this,     SLOT (  downbuttonclicked()) );
 
     m_font.setFamily("Ubuntu");
     m_font.setBold( true );
     setLabelPos(-24,-28 );
+
+    Simulator::self()->addToUpdateList( this );
 
     addPropGroup( { tr("Main"), {
 new DoubProp<Ds18b20>( "Temp"   , tr("Temperature")    ,"°C", this, &Ds18b20::temp   , &Ds18b20::setTemp ),
@@ -101,12 +156,19 @@ void Ds18b20::stamp()   // Called at Simulation Start
     m_lastTime = 0;
     m_rxReg = 0;
     m_lastBit = 7;
-    m_reset = false;
     m_write = false;
     m_lastIn = false;
     m_inpin->setPinMode( openCo );
     m_inpin->setOutState( true );
     m_inpin->changeCallBack( this, true );
+}
+
+void Ds18b20::updateStep()
+{
+    if( !m_changed ) return;
+    m_changed = false;
+    setTemp( m_temp );
+    update();
 }
 
 void Ds18b20::voltChanged()                              // Called when Input Pin changes
@@ -117,18 +179,23 @@ void Ds18b20::voltChanged()                              // Called when Input Pi
     {
         uint64_t time = Simulator::self()->circTime()-m_lastTime; // in picoseconds
 
-        if( time > 480*1e6 )                              // > 480 us : Reset
+        if( time > 8*TSLOT )             // > 480 us : Reset
         {
-            reset();
+            qDebug() << "\nDs18b20::voltChanged -------------- RESET";
+            m_rxReg = 0;
+            m_bitIndex = 0;
+            m_state = W1_COMMAND;
+            m_write = false;
+            pulse( 30, 80 ); // Send 80 us ( 60 to 240 us) pulse after 30 us ( 15 to 60 us)
         }
-        else if( m_reset )
+        else if( m_state > W1_IDLE )    // Active
         {
-            if( time > 60*1e6 )                         // > 60 us : 0 received
+            if( time > 60*1e6 )         // > 60 us : 0 received
             {
                 if( m_write ) {;} // ERROR Master is reading data, Read pulse should be < 15 us
                 else          readBit( 0 );
             }
-            else if( time < 15*1e6 )                         // < 15 us : 1 received Or Read pulse
+            else    ///// if( time < 15*1e6 )     // < 15 us : 1 received Or Read pulse
             {
                 if( m_write ) writeBit();
                 else          readBit( 1 );
@@ -149,86 +216,291 @@ void Ds18b20::runEvent()
     else              // Release
     {
         m_inpin->sheduleState( true, 0 );
-        if( !m_reset ) m_reset = true;
         m_inpin->changeCallBack( this, true ); // Receive voltChange() CallBacks again
     }
 }
 
-void Ds18b20::dataReceived() // a byte has been received (it's in m_rxReg)
-{
-    qDebug() <<"Ds18b20::dataReceived"<< m_lastBit+1<< "bits :"<< m_rxReg; // Print received byte in bottom panel
-    // Do whatever
-}
-
-void Ds18b20::dataSent() // Last data has been sent
-{
-    // Do whatever
-}
-
-void Ds18b20::sendData( uint8_t data, uint8_t size )
+void Ds18b20::sendData( uint8_t data, int size )
 {
     m_txReg = data;
+    m_bitIndex = 0;
     m_lastBit = size-1;
-    m_bit = 1;
     m_write = true;
-    m_pulse = 15*1e6;  // Keep line low for 15 us
 }
 
 void Ds18b20::writeBit()
 {
-    if( (m_txReg & m_bit) == 0 )
+    if( (m_txReg & 1<<m_bitIndex) == 0 )  pulse( 1, 50 ); // Send a 50 us pulse after 1 us
+
+    if( m_bitIndex == m_lastBit ) // Byte sent
     {
-        Simulator::self()->addEvent( 1*1e6, this ); // Pull down in 1 us
-        m_pullDown = true;
-        m_inpin->changeCallBack( this, false ); // Stop receiving voltChange() CallBacks
-    }
-    if( m_bit == 1<<m_lastBit ) // Byte sent
-    {
+        m_bitIndex = 0;
         m_write = false;
         dataSent();
     }
-    else m_bit <<= 1;
+    else m_bitIndex += 1;
+}
+
+void Ds18b20::dataSent() // Last data has been sent
+{
+    if( m_state != W1_SEARCH ) QDeb_HEX( "Ds18b20::dataSent", &m_txReg, 1 );
+
+    if( m_state == W1_SEARCH )
+    {
+        m_lastBit = 0; // Read 1 bit
+    }
+    else if( !tx_BUFF.empty() )   // Send next byte in Tx Buffer
+    {
+        tx_BUFF.pop_back(); // Remove last sent byte
+        if( !tx_BUFF.empty() ) sendData( tx_BUFF.back() ); // Send last byte in list, if list is not empty
+    }
 }
 
 void Ds18b20::readBit( uint8_t bit )
 {
-    if( bit ) m_rxReg |= m_bit;
-
-    if( m_bit == 1<<m_lastBit )          // Complete data received
+    if( m_state == W1_MATCH )  // Compare bit received with ROM
     {
-        m_bit = 1;
+        if( (bit>0) != bitROM( m_bitIndex ) )
+        {
+            m_state = W1_IDLE; // No ROM match, we are out
+            qDebug() <<"Ds18b20::readBit -------- Error -------- NO ROM match"<<m_bitIndex;
+        }
+    }
+    else if( bit ) m_rxReg |= 1<<m_bitIndex;
+
+    if( m_bitIndex == m_lastBit )   // Complete byte received
+    {
+        m_bitIndex = 0;
+        m_lastBit = 7;   // Return to "byte" mode
         dataReceived();
         m_rxReg = 0;
     }
-    else m_bit <<= 1;
+    else m_bitIndex += 1;
 }
 
-void Ds18b20::reset()
+void Ds18b20::dataReceived() // Complete data has been received (it's in m_rxReg)
 {
-    m_rxReg = 0;
-    m_bit = 1;
-    m_reset = false;
-    m_write = false;
+    if( m_state != W1_MATCH && m_state != W1_SEARCH ) QDeb_HEX( "Ds18b20::dataReceived", &m_rxReg, 1 );
+
+    switch( m_state )
+    {
+        case W1_IDLE: break;                      // Error, this shoild not happen
+        case W1_COMMAND: command( m_rxReg ); break; // Command received
+        case W1_DATA: break;
+        case W1_MATCH:                            // ROM Match: we are online, wait for commands
+        {
+            m_state = W1_COMMAND;
+            qDebug() <<"Ds18b20::dataReceived  :  ROM match!!";
+        }break;
+        case W1_SEARCH:
+        {
+            m_bitSearch++;
+            if( m_bitSearch == 64 )    // We passed Search ROM: wait for commands
+            {
+                m_state = W1_IDLE;
+                m_lastBit = 7; // Return to normal byte reception
+                qDebug() << "Ds18b20::dataReceived  :  Search ROM OK";
+            }
+            else if((m_rxReg > 0) == m_bitROM ) sendSearchBit();   // Bit Match,  keep sending
+            else                                m_state = W1_IDLE; // We are out, Wait for next Reset signal
+        }
+    }
+}
+
+void Ds18b20::pulse( uint64_t time, uint64_t witdth ) // Time in us
+{
     m_pullDown = true;
-    m_pulse = 80*1e6;                            // Keep line low for 80 us (60 to 240 us)
-    m_inpin->changeCallBack( this, false );      // Stop receiving voltChange() CallBacks
-    Simulator::self()->addEvent( 30*1e6, this ); // Send ack after 30 us (15 to 60 us)
+    m_pulse = witdth*1e6;                          // Keep line low for width us
+    m_inpin->changeCallBack( this, false );        // Stop receiving voltChange() CallBacks
+    Simulator::self()->addEvent( time*1e6, this ); // Send pulse after time us
+}
+
+// COMMANDS --------------------------------------------------
+
+void Ds18b20::command( uint8_t cmd )
+{
+    m_state = W1_DATA;
+    switch( cmd )
+    {
+        case 0x33: readROM();         break;
+        case 0x44: convertTemp();     break;
+        case 0x48: qDebug() << "Warning:  COPY SCRATCHPAD Not implemented"; break;
+        case 0x4E: qDebug() << "Warning:  WRITE SCRATCHPAD Not implemented"; break;
+        case 0x55: matchROM();        break;
+        case 0xB4: readPowerSupply(); break;
+        case 0xBE: readScratchpad();  break;
+        case 0xCC: skipROM();         break;
+        case 0xF0: searchROM();       break;
+
+        default:   qDebug() << "command Not implemented";
+    }
+    m_lastCommand = cmd;
+}
+
+void Ds18b20::readROM() // Code: 33h : send ROM to Master
+{
+    QDeb_HEX("Ds18b20::readROM", m_ROM,8);
+
+    tx_BUFF.clear();
+    for( int i=7; i>=0; i-- ) tx_BUFF.push_back( m_ROM[i] );
+
+    QDeb_HEX("Ds18b20::readROM tx Buffer:", tx_BUFF);
+
+    sendData( tx_BUFF.back() );
+}
+
+void Ds18b20::convertTemp() // Code 44h, temperature already in the Scratchpad, nothing to do
+{
+    qDebug() << "Ds18b20::convertTemp";
+    m_state = W1_IDLE;
+
+    if( !m_parPower ) pulse( 1, 749 ); // Send a 749 us pulse after 1 us
+}
+
+void Ds18b20::matchROM() // Code 55h : read 64 bits and compare with ROM
+{
+    qDebug() << "Ds18b20::matchROM";
+    m_lastBit = 63;
+    m_state = W1_MATCH;
+}
+
+void Ds18b20::readPowerSupply() // Code B4h : using parasite power? pull down time???
+{
+    qDebug() << "Ds18b20::readPowerSupply"; // By now we don't use parasite power
+    /// TODO add property
+}
+
+void Ds18b20::readScratchpad() // Code BEh send Scratchpad LSB
+{
+    tx_BUFF.clear();
+    for( int i=8; i>=0; i-- ) tx_BUFF.push_back( m_scratchpad[i] );
+
+    QDeb_HEX("Ds18b20::readScratchpad :", tx_BUFF);
+
+    sendData( tx_BUFF.back() );
+}
+
+void Ds18b20::skipROM() // Code: CCh : This device is selected, Wait command
+{
+    qDebug() << "Ds18b20::skipROM";
+    m_state = W1_COMMAND;
+}
+
+void Ds18b20::searchROM() // Code F0h
+{
+    qDebug() << "Ds18b20::searchROM";
+    m_state = W1_SEARCH;
+    m_bitSearch = 0;
+    sendSearchBit();
+}
+
+void Ds18b20::sendSearchBit()
+{
+    m_bitROM = bitROM( m_bitSearch );
+    uint8_t data = m_bitROM ? 1 : 2;
+    sendData( data, 2 );
+}
+
+bool Ds18b20::bitROM( uint bitIndex )
+{
+    uint byte = bitIndex/8;
+    uint bit  = bitIndex%8;
+    return ( m_ROM[ byte ] & 1<<bit ) > 0;
+}
+
+// End Commands --------------------------------------------------------------------
+
+void Ds18b20::setTemp( double t ) // This should convert temp wrote in UI
+{
+    m_temp = t;
+    uint16_t temp = abs( m_temp) * 16.0; // Make it positive and shift to make integer value
+    if(  m_temp < 0 ) temp = ~temp + 1;  // Temp under 0? Make 2nd complement
+
+    //-10.125    °C 1111 1111 0101 1110 FF5Eh
+    //-25.0625   °C 1111 1110 0110 1111 FF6Fh
+
+    /// TODO alarm settings
+    //m_TH_reg =
+    //m_TL_reg =
+
+    // store temperature to scratchpad
+    m_scratchpad[0] = temp;
+    m_scratchpad[1] = temp >> 8;
+    m_scratchpad[2] = m_TH_reg;
+    m_scratchpad[3] = m_TL_reg;
+    m_scratchpad[4] = m_CFG_reg;
+    m_scratchpad[5] = 0xFF; // Reserved. Default?
+    m_scratchpad[6] = 0x0C; // Reserved. Default?
+    m_scratchpad[7] = 0x10; // Reserved. Default?
+    m_scratchpad[8] = crc8( m_scratchpad, 8 );
+
+    QDeb_HEX("Ds18b20::setTemp", m_scratchpad, 9 );
+
+    update();
+}
+
+uint8_t crc8( uint8_t* addr, uint8_t len ) // DS18B20 crc8 calc
+{
+    uint8_t crc = 0;
+
+    while( len--)
+    {
+        uint8_t inbyte = *addr++;
+
+        for( uint8_t i=8; i; i--)
+        {
+            uint8_t mix = (crc ^ inbyte) & 0x01;
+            crc >>= 1;
+
+            if( mix) crc ^= 0x8C;
+            inbyte >>= 1;
+        }
+    }
+    return crc;
+}
+
+void generateROM( uint8_t familyCode, uint8_t* ROM ) // DS18B20 - Generate unique ROM address
+{
+  // 8-bit CRC | 48 bit S/N | 8-BIT FAMILY CODE
+  // MSB - LSB
+  // FAMILY CODE = 28h for DS18B20
+
+  ROM[0] = familyCode;
+  ROM[1] = rand();
+  ROM[2] = rand();
+  ROM[3] = rand();
+  ROM[4] = rand();
+  ROM[5] = 0x00;
+  ROM[6] = 0x00;
+  /*
+  // For test : 28 CA CB E2 03 00 00 B6
+
+  ROM[0] = 0x28;
+  ROM[1] = 0xCA;
+  ROM[2] = 0xCB;
+  ROM[3] = 0xE2;
+  ROM[4] = 0x03;
+  ROM[5] = 0x00;
+  ROM[6] = 0x00;
+  */
+
+  ROM[7] = crc8(ROM, 7);
+
+  QDeb_HEX("Ds18b20::generateROM",ROM,8);
 }
 
 void Ds18b20::upbuttonclicked()
 {
     m_temp += m_tempInc;
-    if( m_temp > 125 )  m_temp = 125;
+    if( m_temp > 125 ) m_temp = 125;
     m_changed = true;
-    update();
 }
 
 void Ds18b20::downbuttonclicked()
 {
-    m_temp -= m_tempInc;
+    m_temp = m_temp - m_tempInc;
     if( m_temp < -55 ) m_temp = -55;
     m_changed = true;
-    update();
 }
 
 void Ds18b20::paint( QPainter* p, const QStyleOptionGraphicsItem* option, QWidget* widget )
@@ -240,11 +512,11 @@ void Ds18b20::paint( QPainter* p, const QStyleOptionGraphicsItem* option, QWidge
     p->drawRoundedRect( m_area, 2, 2 );
 
     p->setBrush( QColor(200, 220, 180) );
-    p->drawRoundedRect( QRect(-24,-14, 48, 15 ),2,2 );
+    p->drawRoundedRect( QRect(-24,-14, 60, 15 ),2,2 );
 
     m_font.setPixelSize(9);
     p->setFont( m_font );
-    p->setPen( QColor(0, 0, 0) );
+    p->setPen( QColor(0, 0, 0) );    
     p->drawText( -22, -3, "C° "+QString::number( m_temp ) );
 }
 
--- src/gui/circuitwidget/components/mcu/ds18b20.h
+++ src/gui/circuitwidget/components/mcu/ds18b20.h
@@ -36,16 +36,25 @@ class MAINMODULE_EXPORT Ds18b20 : public Component , public eElement
         Ds18b20( QObject* parent, QString type, QString id );
         ~Ds18b20();
 
-        static Component* construct( QObject* parent, QString type, QString id );
-        static LibraryItem* libraryItem();
+        enum w1State_t{
+            W1_IDLE=0,
+            W1_COMMAND,
+            W1_DATA,
+            W1_MATCH,
+            W1_SEARCH
+        };
+
+ static Component* construct( QObject* parent, QString type, QString id );
+ static LibraryItem* libraryItem();
 
         double tempInc() { return m_tempInc; }
         void setTempInc( double inc ) { m_tempInc = trim( inc ); }
 
         double temp() { return m_temp; }
-        void setTemp( double temp ) { m_temp = temp; }
+        void setTemp( double t );
 
-        virtual void stamp();
+        virtual void stamp() override;
+        virtual void updateStep() override;
         virtual void voltChanged() override;
         virtual void runEvent() override;
 
@@ -58,23 +67,43 @@ class MAINMODULE_EXPORT Ds18b20 : public Component , public eElement
     private:
         void dataReceived();
         void dataSent();
-        void sendData( uint8_t data, uint8_t size );
+        void sendData( uint8_t data, int size=8);
         void writeBit();
         void readBit( uint8_t bit );
-        void reset();
+        void pulse( uint64_t time, uint64_t witdth ); // Time in us
+        void command( uint8_t cmd );
 
-        double trim( double data ) { return (double)((int)(data*10))/10; }
+        // Commands
+        void skipROM();         // CCh
+        void readROM();         // 33h
+        void convertTemp();     // 44h
+        void readPowerSupply(); // B4h
+        void readScratchpad();  // BEh
+        void matchROM();        // 55h
+        void searchROM();       // F0h
+
+        void sendSearchBit();
+        bool bitROM( uint bitIndex );
+
+        w1State_t m_state;
+        uint8_t m_lastCommand;
+
+        // Trim to 4 decimals, not one! For 12-bit resolution step is 0.0625
+        // For 9-12-bit resolution, steps are acutually 0.5°C, 0.25°C, 0.125°C and 0.0625°C, respectively
+        double trim( double data ) { return (double)((int)(data*10000))/10000; }
 
         bool m_lastIn;
         bool m_write;
-        bool m_reset;
         bool m_pullDown;
+        bool m_bitROM;
+        bool m_parPower;
 
         uint64_t m_lastTime;
         uint8_t  m_rxReg;
         uint8_t  m_txReg;
-        uint8_t  m_bit;
         uint8_t  m_lastBit;
+        uint8_t  m_bitSearch;
+        uint64_t  m_bitIndex;
 
         int m_pulse;
 
@@ -82,8 +111,47 @@ class MAINMODULE_EXPORT Ds18b20 : public Component , public eElement
         double m_tempInc;
 
         QFont m_font;
-
         IoPin* m_inpin;
+
+    // NEW
+        int m_resolution; // 9-12 bits
+        uint8_t m_scratchpad[9];
+        uint8_t m_ROM[8];
+
+        uint8_t m_TH_reg;  // TH register, alarm trigger register, can be stored in internal EEPROM
+        uint8_t m_TL_reg;  // TL register, alarm trigger register, can be stored in internal EEPROM
+        uint8_t m_CFG_reg; // Config register, can be stored in internal EEPROM
+
+        std::vector<uint8_t> tx_BUFF;
+        //std::vector<uint8_t> rx_BUFF;
+
+        ////////////////////////////////////
+        // Constants for 1-Wire bit-banging
+        ////////////////////////////////////
+
+        // "TIME SLOT", here TSLOT last 60us, minimum
+        // For DS1820 is minimum 60us up to 120us
+        // Arduino 1-Wire lib uses 60-65us
+        const double TSLOT  = 60*1e6;
+
+        // Time to write bit 0, pulling line down
+        // It's minimum lenght is the same as TSLOT
+        // For DS1820 is minimum 60us up to 120us
+        // Arduino 1-Wire lib uses 60-65us
+        const double TLOW_0 = TSLOT;
+
+        // TLOW1 is duration to pulling line down for bit 1
+        // For DS1820 is between 1 to 15us
+        // Arduino 1-Wire lib uses 10us
+        const double TLOW_1 = 1*1e6;
+
+        // TREC is recovery time between pulses,
+        // pulling up line minimum time
+        // For DS1820 is minimum 1us up to 15us
+        // Arduino 1-Wire lib uses 5us
+        const double TREC = 5*1e6;
+
+        //const double TRVD
 };
 
 #endif
