bf83f2f6847782d6f04359c27e4b01587e878b6b
arcachofo <arcachofo@simulide.com>
2022-05-31 13:17:31 +0100

    6502: All instructions implemented.

--- src/mcusim/cores/mcs65/mcs65core.cpp
+++ src/mcusim/cores/mcs65/mcs65core.cpp
@@ -79,8 +79,7 @@ void Mcs65Core::Read( uint16_t addr )
             case addr_IMME: m_op0 = m_mcu->extMem->getData(); break; // Immediate: From Mem 1 byte
             case addr_INDI: break;
             case addr_RELA: break;
-        }
-    }
+    }   }
     PC++;
 }
 
@@ -91,13 +90,12 @@ void Mcs65Core::Write( uint16_t addr, uint8_t val )
         case addr_NONE:                            break; /// ERROR
         case addr_ACCU: ACC = val;                 break;
         default: m_mcu->extMem->write( addr, val );
-    }
-}
+}   }
 
 void Mcs65Core::pushStack8( uint8_t byte ) { Write( 0x0100 + m_SP, byte ); m_SP--; }
 void Mcs65Core::popStack8()                { m_SP++; Read( 0x0100 + m_SP ); }
 
-void Mcs65Core::Op_ADC()
+void Mcs65Core::ADC()
 {
     uint8_t m = m_op0;
     unsigned int tmp = m + ACC + (STATUS(C) ? 1 : 0);
@@ -120,21 +118,17 @@ void Mcs65Core::Op_ADC()
     ACC = tmp & 0xFF;
 }
 
-void Mcs65Core::Op_AND() { ACC &= m_op0; setNZ( ACC ); }
+void Mcs65Core::AND() { ACC &= m_op0; setNZ( ACC ); }
 
-void Mcs65Core::Op_ASL()
+void Mcs65Core::ASL()
 {
     SET_CARRY( m_op0 & 0x80 );
     m_op0 <<= 1;
     setNZ( m_op0 );
     Write( m_readAddr, m_op0 );
 }
-/// +1 cycle same page +2 different page
-void Mcs65Core::Op_BCC() { if(!STATUS(C) ) PC = m_op0; } // Branch on Carry Clear : 1b 2c+1|2
-void Mcs65Core::Op_BCS() { if( STATUS(C) ) PC = m_op0; } // Branch on Carry Set   : 1b 2c+1|2
-void Mcs65Core::Op_BEQ() { if( STATUS(Z) ) PC = m_op0; } // Branch on Result Zero : 1b 2c+1|2
 
-void Mcs65Core::Op_BIT()
+void Mcs65Core::BIT()
 {
     uint8_t res = m_op0 & ACC;
     SET_NEGATIVE( res );
@@ -142,11 +136,7 @@ void Mcs65Core::Op_BIT()
     SET_ZERO( res == 0 );
 }
 
-void Mcs65Core::Op_BMI() { if( STATUS(N) ) PC = m_op0; }
-void Mcs65Core::Op_BNE() { if(!STATUS(Z) ) PC = m_op0; }
-void Mcs65Core::Op_BPL() { if(!STATUS(N) ) PC = m_op0; }
-
-void Mcs65Core::Op_BRK() // Break/Interrupt : 1 byte, 7 cycles
+void Mcs65Core::BRK() // Break/Interrupt : 1 byte, 7 cycles
 {SET_INTERRUPT( 1 );
     switch( m_step )
     {
@@ -156,34 +146,30 @@ void Mcs65Core::Op_BRK() // Break/Interrupt : 1 byte, 7 cycles
         case 3: Read( m_IsrH );                break; // Read IsrH
         case 4: Read( m_IsrL ); m_op1 = m_op0; break; // Read IsrL
         case 5: PC = (m_op1 << 8) + m_op0;            // Jump to Isr
-    }
-}
+}   }
 
-void Mcs65Core::Op_BVC() { if(!STATUS(O) ) PC = m_op0; }
-void Mcs65Core::Op_BVS() { if( STATUS(O) ) PC = m_op0; }
+void Mcs65Core::CLC() { SET_CARRY(0);     }
+void Mcs65Core::CLD() { SET_DECIMAL(0);   }
+void Mcs65Core::CLI() { SET_INTERRUPT(0); }
+void Mcs65Core::CLV() { SET_OVERFLOW(0);  }
 
-void Mcs65Core::Op_CLC() { SET_CARRY(0);     }
-void Mcs65Core::Op_CLD() { SET_DECIMAL(0);   }
-void Mcs65Core::Op_CLI() { SET_INTERRUPT(0); }
-void Mcs65Core::Op_CLV() { SET_OVERFLOW(0);  }
+void Mcs65Core::CMP() { SET_CARRY(  ACC    >= m_op0 ); setNZ(  ACC    - m_op0 ); }
+void Mcs65Core::CPX() { SET_CARRY( *m_regX >= m_op0 ); setNZ( *m_regX - m_op0 ); }
+void Mcs65Core::CPY() { SET_CARRY( *m_regY >= m_op0 ); setNZ( *m_regY - m_op0 ); }
 
-void Mcs65Core::Op_CMP() { SET_CARRY(  ACC    >= m_op0 ); setNZ(  ACC    - m_op0 ); }
-void Mcs65Core::Op_CPX() { SET_CARRY( *m_regX >= m_op0 ); setNZ( *m_regX - m_op0 ); }
-void Mcs65Core::Op_CPY() { SET_CARRY( *m_regY >= m_op0 ); setNZ( *m_regY - m_op0 ); }
+void Mcs65Core::DEC() {  m_op0  -= 1; setNZ( m_op0 );  Write( m_readAddr, m_op0 ); }
+void Mcs65Core::DEX() { *m_regX -= 1; setNZ( *m_regX ); }
+void Mcs65Core::DEY() { *m_regY -= 1; setNZ( *m_regY ); }
 
-void Mcs65Core::Op_DEC() {  m_op0  -= 1; setNZ( m_op0 );  Write( m_readAddr, m_op0 ); }
-void Mcs65Core::Op_DEX() { *m_regX -= 1; setNZ( *m_regX ); }
-void Mcs65Core::Op_DEY() { *m_regY -= 1; setNZ( *m_regY ); }
+void Mcs65Core::EOR() { ACC = ACC ^ m_op0; setNZ( m_op0 ); }
 
-void Mcs65Core::Op_EOR() { ACC = ACC ^ m_op0; setNZ( m_op0 ); }
+void Mcs65Core::INC() {  m_op0  += 1; setNZ( m_op0 ); Write( m_readAddr, m_op0 ); }
+void Mcs65Core::INX() { *m_regX += 1; setNZ( *m_regX ); }
+void Mcs65Core::INY() { *m_regY += 1; setNZ( *m_regY ); }
 
-void Mcs65Core::Op_INC() {  m_op0  += 1; setNZ( m_op0 ); Write( m_readAddr, m_op0 ); }
-void Mcs65Core::Op_INX() { *m_regX += 1; setNZ( *m_regX ); }
-void Mcs65Core::Op_INY() { *m_regY += 1; setNZ( *m_regY ); }
+void Mcs65Core::JMP() { PC = m_op0; }
 
-void Mcs65Core::Op_JMP() { PC = m_op0; }
-
-void Mcs65Core::Op_JSR()                    /////////////////
+void Mcs65Core::JSR()                    /////////////////
 {
     PC--;
     pushStack8( (PC >> 8) & 0xFF);
@@ -191,11 +177,11 @@ void Mcs65Core::Op_JSR()                    /////////////////
     PC = m_op0;
 }
 
-void Mcs65Core::Op_LDA() { ACC     = m_op0; setNZ( m_op0 ); }
-void Mcs65Core::Op_LDX() { *m_regX = m_op0; setNZ( m_op0 ); }
-void Mcs65Core::Op_LDY() { *m_regY = m_op0; setNZ( m_op0 ); }
+void Mcs65Core::LDA() { ACC     = m_op0; setNZ( m_op0 ); }
+void Mcs65Core::LDX() { *m_regX = m_op0; setNZ( m_op0 ); }
+void Mcs65Core::LDY() { *m_regY = m_op0; setNZ( m_op0 ); }
 
-void Mcs65Core::Op_LSR()
+void Mcs65Core::LSR()
 {
     SET_CARRY( m_op0 & 0x01 );
     m_op0 >>= 1;
@@ -203,27 +189,27 @@ void Mcs65Core::Op_LSR()
     Write( m_readAddr, m_op0 );
 }
 
-void Mcs65Core::Op_NOP() { return; }
+void Mcs65Core::NOP() { return; }
 
-void Mcs65Core::Op_ORA() { ACC |= m_op0; setNZ( ACC ); }
+void Mcs65Core::ORA() { ACC |= m_op0; setNZ( ACC ); }
 
-void Mcs65Core::Op_PHA() { pushStack8( ACC ); }
-void Mcs65Core::Op_PHP() { pushStack8( STATUS(C) | STATUS(B) ); }
+void Mcs65Core::PHA() { pushStack8( ACC ); }
+void Mcs65Core::PHP() { pushStack8( STATUS(C) | STATUS(B) ); }
 
-void Mcs65Core::Op_PLA()
+void Mcs65Core::PLA()
 {
     if( m_step == 0 ) { popStack8(); m_cpuState = cpu_EXEC; return; }
     ACC = m_op0;
     setNZ( ACC );
 }
 
-void Mcs65Core::Op_PLP()
+void Mcs65Core::PLP()
 {
     if( m_step == 0 ) { popStack8(); m_cpuState = cpu_EXEC; }
     else              *m_STATUS = m_op0 | CONSTANT | BREAK;
 }
 
-void Mcs65Core::Op_ROL()
+void Mcs65Core::ROL()
 {
     bool carry = m_op0 & 0x80;
     m_op0 <<= 1;
@@ -233,7 +219,7 @@ void Mcs65Core::Op_ROL()
     Write( m_readAddr, m_op0 );
 }
 
-void Mcs65Core::Op_ROR()
+void Mcs65Core::ROR()
 {
     bool carry = m_op0 & 1;
     m_op0 >>= 1;
@@ -243,7 +229,7 @@ void Mcs65Core::Op_ROR()
     Write( m_readAddr, m_op0 );
 }
 
-void Mcs65Core::Op_RTI() // Return from Interrupt
+void Mcs65Core::RTI() // Return from Interrupt
 {
     switch( m_step )
     {
@@ -251,20 +237,18 @@ void Mcs65Core::Op_RTI() // Return from Interrupt
         case 1: popStack8(); m_cpuState = cpu_EXEC; *m_STATUS = m_op0 | CONSTANT | BREAK; break;
         case 2: popStack8(); m_cpuState = cpu_EXEC; m_op1 = m_op0; break;
         case 3: PC = (m_op0 << 8) | m_op1;
-    }
-}
+}   }
 
-void Mcs65Core::Op_RTS() // Return from Subroutine
+void Mcs65Core::RTS() // Return from Subroutine
 {
     switch( m_step )
     {
         case 0: popStack8(); m_cpuState = cpu_EXEC; break;
         case 1: popStack8(); m_cpuState = cpu_EXEC; m_op1 = m_op0; break;
         case 2: PC = ( (m_op0 << 8) | m_op1 ) + 1;
-    }
-}
+}   }
 
-void Mcs65Core::Op_SBC()
+void Mcs65Core::SBC()
 {
     uint8_t m = m_op0;
     uint tmp = ACC - m - (STATUS(C) ? 0 : 1);
@@ -281,54 +265,35 @@ void Mcs65Core::Op_SBC()
     ACC = ( tmp & 0xFF );
 }
 
-void Mcs65Core::Op_SEC() { SET_CARRY(1); }
-void Mcs65Core::Op_SED() { SET_DECIMAL(1); }
-void Mcs65Core::Op_SEI() { SET_INTERRUPT(1); }
+void Mcs65Core::SEC() { SET_CARRY(1); }
+void Mcs65Core::SED() { SET_DECIMAL(1); }
+void Mcs65Core::SEI() { SET_INTERRUPT(1); }
 
-void Mcs65Core::Op_STA() { Write( m_op0,  ACC ); }
-void Mcs65Core::Op_STX() { Write( m_op0, *m_regX ); }
-void Mcs65Core::Op_STY() { Write( m_op0, *m_regY ); }
+void Mcs65Core::STA() { Write( m_op0,  ACC ); }
+void Mcs65Core::STX() { Write( m_op0, *m_regX ); }
+void Mcs65Core::STY() { Write( m_op0, *m_regY ); }
 
-void Mcs65Core::Op_TAX() { *m_regX = ACC; setNZ( ACC ); }
-void Mcs65Core::Op_TAY() { *m_regY = ACC; setNZ( ACC ); }
+void Mcs65Core::TAX() { *m_regX = ACC; setNZ( ACC ); }
+void Mcs65Core::TAY() { *m_regY = ACC; setNZ( ACC ); }
 
-void Mcs65Core::Op_TXA() { ACC = *m_regX; setNZ( ACC ); }
-void Mcs65Core::Op_TYA() { ACC = *m_regY; setNZ( ACC ); }
+void Mcs65Core::TXA() { ACC = *m_regX; setNZ( ACC ); }
+void Mcs65Core::TYA() { ACC = *m_regY; setNZ( ACC ); }
 
-void Mcs65Core::Op_TSX() { *m_regX = m_SP; setNZ( m_SP ); }
-void Mcs65Core::Op_TXS() { m_SP = *m_regX; }
+void Mcs65Core::TSX() { *m_regX = m_SP; setNZ( m_SP ); }
+void Mcs65Core::TXS() { m_SP = *m_regX; }
 
-void  Mcs65Core::BXX( uint8_t flag, uint8_t y )
+void  Mcs65Core::BXX( uint8_t flag, uint8_t y ) /// +1 cycle same page +2 different page
 {
-
+    uint8_t bit = 0;
+    switch( flag ) {
+        case 0: bit = N; break;
+        case 1: bit = O; break;
+        case 2: bit = C; break;
+        case 3: bit = Z; break;
+    }
+    uint8_t flagVal = STATUS( bit ) ? 1 : 0;
+    if( flagVal == y ) PC = m_op0;
 }
-void Mcs65Core::BRK(){;}
-void Mcs65Core::PHP(){;}
-void Mcs65Core::BPL(){;}
-void Mcs65Core::CLC(){;}
-void Mcs65Core::JSR(){;}
-void Mcs65Core::PLP(){;}
-void Mcs65Core::SEC(){;}
-void Mcs65Core::RTI(){;}
-void Mcs65Core::PHA(){;}
-void Mcs65Core::CLI(){;}
-void Mcs65Core::RTS(){;}
-void Mcs65Core::PLA(){;}
-void Mcs65Core::SEI(){;}
-void Mcs65Core::DEY(){;}
-void Mcs65Core::TXA(){;}
-void Mcs65Core::TYA(){;}
-void Mcs65Core::TXS(){;}
-void Mcs65Core::TAY(){;}
-void Mcs65Core::TAX(){;}
-void Mcs65Core::CLV(){;}
-void Mcs65Core::TSX(){;}
-void Mcs65Core::INY(){;}
-void Mcs65Core::DEX(){;}
-void Mcs65Core::CLD(){;}
-void Mcs65Core::INX(){;}
-void Mcs65Core::NOP(){;}
-void Mcs65Core::SED(){;}
 
 void Mcs65Core::runDecoder()
 {
@@ -384,7 +349,6 @@ if( m_group == 1 ){ m_addrMode = addr_ABSO; m_regI = m_regY;                    
         switch( m_instr ) {                 // Irregular m_instrs
             case 0x00: BRK(); break; // BRK
             case 0x08: PHP(); break; // PHP
-            case 0x10: BPL(); break; // BPL
             case 0x18: CLC(); break; // CLC
             case 0x20: JSR(); break; // JSR abs
             case 0x28: PLP(); break; // PLP
@@ -412,48 +376,45 @@ if( m_group == 1 ){ m_addrMode = addr_ABSO; m_regI = m_regY;                    
             default:{
                 switch( m_group ){               // Regular m_instrs
                     case 0:{
-                    if( m_Amode == 4 ) // xxy 100 00 conditional branch
-                    {
-                        uint8_t flag = (m_instr & 0b11000000) >> 6; // 0=N, 1=O, 2=C, 3=Z
-                        uint8_t y    = (m_instr & 0b00100000) >> 5;
-                        BXX( flag, y ); // BPL BMI BVC BVS BCC BCS BNE BEQ
-                    }else{
-                        switch( m_Ocode ){
-                            case 0: break;
-                            case 1: Op_BIT(); break;
-                            case 2: //Op_JMP();
-                            case 3: Op_JMP(); break;
-                            case 4: Op_STY(); break;
-                            case 5: Op_LDY(); break;
-                            case 6: Op_CPY(); break;
-                            case 7: Op_CPX(); break;
-                    }   }
-                    }break;
+                        if( m_Amode == 4 ) // xxy 100 00 conditional branch
+                        {
+                            uint8_t flag = (m_instr & 0b11000000) >> 6; // 0=N, 1=O, 2=C, 3=Z
+                            uint8_t y    = (m_instr & 0b00100000) >> 5;
+                            BXX( flag, y ); // BPL BMI BVC BVS BCC BCS BNE BEQ
+                        }else{
+                            switch( m_Ocode ){
+                                case 0: break;
+                                case 1: BIT(); break;
+                                case 2: //JMP();
+                                case 3: JMP(); break;
+                                case 4: STY(); break;
+                                case 5: LDY(); break;
+                                case 6: CPY(); break;
+                                case 7: CPX(); break;
+                        }   }
+                    } break;
                     case 1:{ switch( m_Ocode ){
-                            case 0: Op_ORA(); break;
-                            case 1: Op_AND(); break;
-                            case 2: Op_EOR(); break;
-                            case 3: Op_ADC(); break;
-                            case 4: Op_STA(); break;
-                            case 5: Op_LDA(); break;
-                            case 6: Op_CMP(); break;
-                            case 7: Op_SBC(); break;
+                            case 0: ORA(); break;
+                            case 1: AND(); break;
+                            case 2: EOR(); break;
+                            case 3: ADC(); break;
+                            case 4: STA(); break;
+                            case 5: LDA(); break;
+                            case 6: CMP(); break;
+                            case 7: SBC(); break;
                     } }break;
                     case 2:{ switch( m_Ocode ){
-                            case 0: Op_ASL(); break;
-                            case 1: Op_ROL(); break;
-                            case 2: Op_LSR(); break;
-                            case 3: Op_ROR(); break;
-                            case 4: Op_STX(); break;
-                            case 5: Op_LDX(); break;
-                            case 6: Op_DEC(); break;
-                            case 7: Op_INC(); break;
+                            case 0: ASL(); break;
+                            case 1: ROL(); break;
+                            case 2: LSR(); break;
+                            case 3: ROR(); break;
+                            case 4: STX(); break;
+                            case 5: LDX(); break;
+                            case 6: DEC(); break;
+                            case 7: INC(); break;
                     } }break;
                     case 3: break;
-                }
-            }
-        }
-    }
+    }   }   }   }
 
     if( m_cpuState == cpu_FETCH ) // Start m_instr cycle
     {
--- src/mcusim/cores/mcs65/mcs65core.h
+++ src/mcusim/cores/mcs65/mcs65core.h
@@ -23,21 +23,13 @@
 #include "mcucore.h"
 #include "mcupin.h"
 
-//#define NEGATIVE  0x80
-//#define OVERFLOW  0x40
 #define CONSTANT  0x20
 #define BREAK     0x10
-//#define DECIMAL   0x08
-//#define INTERRUPT 0x04
-//#define ZERO      0x02
-//#define CARRY     0x01
 
 #define ACC       m_acc
 
 #define SET_NEGATIVE(x)  write_S_Bit( N, x & 0x80 ) //(x ? (m_STATUS |= NEGATIVE) : (m_STATUS &= (~NEGATIVE)) )
 #define SET_OVERFLOW(x)  write_S_Bit( V, x ) //(x ? (m_STATUS |= OVERFLOW) : (m_STATUS &= (~OVERFLOW)) )
-//#define SET_CONSTANT(x) (x ? (m_STATUS |= CONSTANT) : (m_STATUS &= (~CONSTANT)) )
-//#define SET_BREAK(x) (x ? (m_STATUS |= BREAK) : (m_STATUS &= (~BREAK)) )
 #define SET_DECIMAL(x)   write_S_Bit( D, x ) //(x ? (m_STATUS |= DECIMAL) : (m_STATUS &= (~DECIMAL)) )
 #define SET_INTERRUPT(x) write_S_Bit( I, x ) //(x ? (m_STATUS |= INTERRUPT) : (m_STATUS &= (~INTERRUPT)) )
 #define SET_ZERO(x)      write_S_Bit( Z, x ) //(x ? (m_STATUS |= ZERO) : (m_STATUS &= (~ZERO)) )
@@ -59,12 +51,6 @@ class MAINMODULE_EXPORT Mcs65Core : public McuCore
             cpu_EXEC,
             cpu_WRITE
         };
-
-        /*enum readState_t{
-            read_IDLE=0,
-            read_READY,
-            read_STEP,
-        };*/
 
         enum addrMode_t{
             addr_NONE=0,
@@ -118,7 +104,6 @@ class MAINMODULE_EXPORT Mcs65Core : public McuCore
         uint8_t m_op0;
         uint8_t m_op1;
         uint16_t m_readAddr;
-        //readState_t m_readState;
 
         bool m_zeroPage;
 
@@ -130,94 +115,54 @@ class MAINMODULE_EXPORT Mcs65Core : public McuCore
         void pushStack8( uint8_t byte );
         void popStack8();
 
-        inline void Op_ADC();
-        inline void Op_AND();
-        inline void Op_ASL();
-        //inline void Op_ASL_ACC();
-        inline void Op_BCC();
-        inline void Op_BCS();
-        inline void Op_BEQ();
-        inline void Op_BIT();
-        inline void Op_BMI();
-        inline void Op_BNE();
-        inline void Op_BPL();
-        inline void Op_BRK();
-        inline void Op_BVC();
-        inline void Op_BVS();
-        inline void Op_CLC();
-        inline void Op_CLD();
-        inline void Op_CLI();
-        inline void Op_CLV();
-        inline void Op_CMP();
-        inline void Op_CPX();
-        inline void Op_CPY();
-        inline void Op_DEC();
-        inline void Op_DEX();
-        inline void Op_DEY();
-        inline void Op_EOR();
-        inline void Op_INC();
-        inline void Op_INX();
-        inline void Op_INY();
-        inline void Op_JMP();
-        inline void Op_JSR();
-        inline void Op_LDA();
-        inline void Op_LDX();
-        inline void Op_LDY();
-        inline void Op_LSR();
-        //inline void Op_LSR_ACC();
-        inline void Op_NOP();
-        inline void Op_ORA();
-        inline void Op_PHA();
-        inline void Op_PHP();
-        inline void Op_PLA();
-        inline void Op_PLP();
-        inline void Op_ROL();
-        //inline void Op_ROL_ACC();
-        inline void Op_ROR();
-        //inline void Op_ROR_ACC();
-        inline void Op_RTI();
-        inline void Op_RTS();
-        inline void Op_SBC();
-        inline void Op_SEC();
-        inline void Op_SED();
-        inline void Op_SEI();
-        inline void Op_STA();
-        inline void Op_STX();
-        inline void Op_STY();
-        inline void Op_TAX();
-        inline void Op_TAY();
-        inline void Op_TSX();
-        inline void Op_TXA();
-        inline void Op_TXS();
-        inline void Op_TYA();
-
-        inline void BXX( uint8_t flag, uint8_t y );
+        inline void ADC();
+        inline void AND();
+        inline void ASL();
+        inline void BIT();
         inline void BRK();
-        inline void PHP();
-        inline void BPL();
         inline void CLC();
-        inline void JSR();
-        inline void PLP();
-        inline void SEC();
-        inline void RTI();
-        inline void PHA();
-        inline void CLI();
-        inline void RTS();
-        inline void PLA();
-        inline void SEI();
-        inline void DEY();
-        inline void TXA();
-        inline void TYA();
-        inline void TXS();
-        inline void TAY();
-        inline void TAX();
-        inline void CLV();
-        inline void TSX();
-        inline void INY();
-        inline void DEX();
         inline void CLD();
+        inline void CLI();
+        inline void CLV();
+        inline void CMP();
+        inline void CPX();
+        inline void CPY();
+        inline void DEC();
+        inline void DEX();
+        inline void DEY();
+        inline void EOR();
+        inline void INC();
         inline void INX();
+        inline void INY();
+        inline void JMP();
+        inline void JSR();
+        inline void LDA();
+        inline void LDX();
+        inline void LDY();
+        inline void LSR();
         inline void NOP();
+        inline void ORA();
+        inline void PHA();
+        inline void PHP();
+        inline void PLA();
+        inline void PLP();
+        inline void ROL();
+        inline void ROR();
+        inline void RTI();
+        inline void RTS();
+        inline void SBC();
+        inline void SEC();
         inline void SED();
+        inline void SEI();
+        inline void STA();
+        inline void STX();
+        inline void STY();
+        inline void TAX();
+        inline void TAY();
+        inline void TSX();
+        inline void TXA();
+        inline void TXS();
+        inline void TYA();
+        inline void BXX( uint8_t flag, uint8_t y );
 };
 #endif
