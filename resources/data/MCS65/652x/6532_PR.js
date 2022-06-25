01eb01c156f9dc43b316df22eee8b225a72ebf47
arcachofo <arcachofo@simulide.com>
2022-06-25 04:47:03 +0100

    Added 6532.

---
+++ resources/data/MCS65/652x/6532_PR.js
@@ -0,0 +1,15 @@
+
+var pra = 0;
+
+function reset()
+{ 
+    //print("resetting PORTA from Script "); 
+}
+
+function configureA( newADDR )
+{
+    print("Address Bus = " + newADDR ); 
+    
+    //pra = newCRA & (1<<2);
+    //print("pra = " + pra ); 
+}
---
+++ resources/data/MCS65/652x/6532_core.js
@@ -0,0 +1,18 @@
+
+//var pra = 0;
+
+function init()
+{
+    
+}
+
+function reset()
+{ 
+    print("resetting 6532 "); 
+}
+
+function runClock( clkState )
+{
+    print("6532 runClock " + clkState ); 
+    
+}
