6facef295238ec238bb09b939770ffdd8bbca32b
arcachofo <arcachofo@simulide.com>
2022-10-20 04:44:00 +0100

    AVR EEPROM: EECR and EEMPE bits are cleared by hardware.

--- src/microsim/cores/avr/avreeprom.cpp
+++ src/microsim/cores/avr/avreeprom.cpp
@@ -8,6 +8,11 @@
 #include "e_mcu.h"
 #include "simulator.h"
 
+#include "avreeprom.h"
+#include "datautils.h"
+#include "e_mcu.h"
+#include "simulator.h"
+
 AvrEeprom::AvrEeprom( eMcu* mcu, QString name )
          : McuEeprom( mcu, name )
 {
@@ -27,36 +32,42 @@ void AvrEeprom::initialize()
 
 void AvrEeprom::runEvent() // Write cycle end reached
 {
-    clearRegBits( m_EEPE );
+    bool eempe = getRegBitsBool( *m_EECR, m_EEMPE );
+    if( eempe ) clearRegBits( m_EEMPE );            // No read operation took place: clear EEMPE
+    else        clearRegBits( m_EEPE );             // Read operation took place: clear EEPE
 }
 
 void AvrEeprom::configureA( uint8_t newEECR ) // EECR is being written
 {
-    m_mode = getRegBitsVal( newEECR, m_EEPM );
-
     bool eempe = getRegBitsBool( newEECR, m_EEMPE );
     bool eepe  = getRegBitsBool( newEECR, m_EEPE );
 
+    if( !eepe )
+    {
+        m_mode = getRegBitsVal( newEECR, m_EEPM ); // EEPROM Programming Mode Bits
+
+        if( getRegBitsBool( newEECR, m_EERE ) ) // Read triggered
+        {
+            m_mcu->m_regOverride = newEECR & ~(m_EERE.mask); // Clear EERE: it happens after 4 cycles, but cpu is halted for these cycles
+            m_mcu->cyclesDone += 4;
+            readEeprom(); // Should we return here?
+        }
+    }
     if( eempe )
     {
         bool oldEepe = getRegBitsBool( *m_EECR, m_EEPE );
 
         if( !oldEepe && eepe ) // Write triggered
         {
-            if( m_mcu->cycle() <= m_nextCycle ) writeEeprom();// write data
+            m_mcu->m_regOverride = newEECR & ~(m_EEMPE.mask); // Clear EEMPE: it happens after 4 cycles but we need to cancel it now
+            Simulator::self()->cancelEvents( this );          // Cancel EEMPE clear event
+            m_mcu->cyclesDone += 2;
+            writeEeprom();
             return;
-            /// else clearRegBits( m_EEPE );
         }
         bool oldEempe = getRegBitsBool( *m_EECR, m_EEMPE );
-        if( !oldEempe ) // Set maximun cycle to procedd to write
-        {
-            m_nextCycle = m_mcu->cycle()+4;
-        }
-    }
-    if( !eepe && getRegBitsBool( newEECR, m_EERE ) ) // Read enable
-    {
-        m_mcu->cyclesDone += 4;
-        readEeprom();
+        if( !oldEempe ) // Shedule EEMPE clear: 4 cycles
+            Simulator::self()->addEvent( m_mcu->psCycle()*4, this );
     }
 }
 
@@ -65,21 +76,26 @@ void AvrEeprom::writeEeprom()
     uint8_t data = *m_dataReg;
     uint64_t time;
 
-    switch( m_mode )
+    if( m_EEPM.mask ) // Has EEPM bits: atmega328 and friends
     {
-        case 0:     // 3.4 ms - Erase and Write in one operation (Atomic Operation)
-            time = 3400*1e6; // picoseconds
-            break;
-        case 1:     // 1.8 ms - Erase Only
-            data = 0xFF;
-            // fallthrough
-        case 2:     // 1.8 ms - Write Only
-            time = 1800*1e6; // picoseconds
-            break;
+        switch( m_mode )
+        {
+            case 0:     // 3.4 ms - Erase and Write in one operation (Atomic Operation)
+                time = 3400*1e6; // picoseconds
+                break;
+            case 1:     // 1.8 ms - Erase Only
+                data = 0xFF;
+                // fallthrough
+            case 2:     // 1.8 ms - Write Only
+                time = 1800*1e6; // picoseconds
+                break;
+        }
+    }
+    else            // No EEPM bits: atmega8 8.5 ms
+    {
+        time = 8500*1e6; // picoseconds
     }
     m_mcu->setRomValue( m_address, data );
-    m_mcu->cyclesDone += 2;
-
     Simulator::self()->addEvent( time, this ); // Shedule Write cycle end
 }
 
