548167d838867e59648b8bfd825641511691008a
Arcachofo <santigoro@mailfence.com>
2024-03-24 13:50:19 +0000

    Script Module base implementation.

--- src/microsim/modules/script/scriptmodule.cpp
+++ src/microsim/modules/script/scriptmodule.cpp
@@ -12,8 +12,88 @@ ScriptModule::ScriptModule( eMcu* mcu, QString name )
             : ScriptBase( mcu->getId()+"-"+"ScriptModule" )
             , McuModule( mcu, name )
 {
-
+    m_configureA   = NULL;
+    m_configureB   = NULL;
+    m_configureC   = NULL;
+    m_callBackDoub = NULL;
+    m_callBack     = NULL;
 }
 ScriptModule::~ScriptModule()
 {
+}
+
+void ScriptModule::setScriptFile( QString scriptFile, bool compile )
+{
+    ScriptBase::setScriptFile( scriptFile, compile );
+    if( compile ) compileScript();
+}
+
+void ScriptModule::setScript( QString script )
+{
+    ScriptBase::setScript( script  );
+}
+
+int ScriptModule::compileScript()
+{
+    if( !m_aEngine ) return -1;
+
+    int r = ScriptBase::compileScript();
+    if( r < 0 ) return r;
+
+    m_configureA   = m_aEngine->GetModule(0)->GetFunctionByDecl("void configureA( uint v )");
+    m_configureB   = m_aEngine->GetModule(0)->GetFunctionByDecl("void configureB( uint v )");
+    m_configureC   = m_aEngine->GetModule(0)->GetFunctionByDecl("void configureC( uint v )");
+    m_callBackDoub = m_aEngine->GetModule(0)->GetFunctionByDecl("void callBackDoub( double v )");
+    m_callBack     = m_aEngine->GetModule(0)->GetFunctionByDecl("void callBack()");
+
+    //m_vChangedCtx = m_voltChanged ? m_aEngine->CreateContext() : NULL;
+
+    asIScriptFunction* func = m_aEngine->GetModule(0)->GetFunctionByDecl("void setup()");
+    if( func ) callFunction( func );
+
+    return 0;
+}
+
+void ScriptModule::configureA( uint8_t v )
+{
+    if( !m_configureA ) return;
+
+    prepare( m_configureA );
+    m_context->SetArgDWord( 0, v );
+    execute();
+}
+
+void ScriptModule::configureB( uint8_t v )
+{
+    if( !m_configureB ) return;
+
+    prepare( m_configureB );
+    m_context->SetArgDWord( 0, v );
+    execute();
+}
+
+void ScriptModule::configureC( uint8_t v )
+{
+    if( !m_configureC ) return;
+
+    prepare( m_configureC );
+    m_context->SetArgDWord( 0, v );
+    execute();
+}
+
+void ScriptModule::callBackDoub( double v )
+{
+    if( !m_callBackDoub ) return;
+
+    prepare( m_callBackDoub );
+    m_context->SetArgDouble( 0, v );
+    execute();
+}
+
+void ScriptModule::callBack()
+{
+    if( !m_callBack ) return;
+
+    prepare( m_callBack );
+    execute();
 }
--- src/microsim/modules/script/scriptmodule.h
+++ src/microsim/modules/script/scriptmodule.h
@@ -10,15 +10,29 @@
 #include "scriptbase.h"
 //#include "e-element.h"
 
-
 class ScriptModule : public ScriptBase, public McuModule //eElement
 {
     public:
         ScriptModule( eMcu* mcu, QString name );
         ~ScriptModule();
 
+        virtual int compileScript() override;
+
+        virtual void setScriptFile( QString scriptFile, bool compile=true ) override;
+        virtual void setScript( QString script ) override;
+
+        virtual void configureA( uint8_t ) override;
+        virtual void configureB( uint8_t ) override;
+        virtual void configureC( uint8_t ) override;
+        virtual void callBackDoub( double )override;
+        virtual void callBack()            override;
 
     protected:
 
+        asIScriptFunction* m_configureA;
+        asIScriptFunction* m_configureB;
+        asIScriptFunction* m_configureC;
+        asIScriptFunction* m_callBackDoub;
+        asIScriptFunction* m_callBack;
 };
 #endif
