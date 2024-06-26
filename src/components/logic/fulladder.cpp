40d78d48f0cae257b4412dacdb93a850d09f7007
Arcachofo <santigoro@mailfence.com>
2024-06-26 20:34:15 +0200

    Full Adder: size configurable.

--- src/components/logic/fulladder.cpp
+++ src/components/logic/fulladder.cpp
@@ -5,7 +5,10 @@
 
 #include "fulladder.h"
 #include "itemlibrary.h"
+#include "circuit.h"
 #include "iopin.h"
+
+#include "intprop.h"
 
 #define tr(str) simulideTr("FullAdder",str)
 
@@ -23,19 +26,21 @@ LibraryItem* FullAdder::libraryItem()
 }
 
 FullAdder::FullAdder( QString type, QString id)
-         : LogicComponent( type, id )
+         : IoComponent( type, id )
+         , eElement( id )
 {
     m_width  = 3;
     m_height = 4;
+    m_bits   = 0;
 
-    init({         // Inputs:
-            "IL01A",
-            "IL03B",
-            "IR01Ci",
-                    // Outputs:
-            "OR02S",
-            "OR03Co",
-        });
+    m_ciPin = createPin("IR01Ci", m_id+"-ci");
+    m_coPin = createPin("OR03Co", m_id+"-co");
+    setBits( 1 );
+
+    addPropGroup( { tr("Main"), {
+        new IntProp <FullAdder>("Bits", tr("Size"),"_bits"
+                            , this, &FullAdder::bits, &FullAdder::setBits, propNoCopy,"uint" ),
+    }, groupNoCopy} );
 
     addPropGroup( { tr("Electric"), IoComponent::inputProps()
                                   + IoComponent::outputProps(),0 } );
@@ -46,21 +51,102 @@ FullAdder::~FullAdder(){}
 
 void FullAdder::stamp()
 {
-    for( uint i=0; i<m_inPin.size(); ++i ) m_inPin[i]->changeCallBack( this );
-    LogicComponent::stamp();
+    for( int i=0; i<m_bits; ++i )
+    {
+        m_inA[i]->changeCallBack( this );
+        m_inB[i]->changeCallBack( this );
+    }
+    m_ciPin->changeCallBack( this );
+
+    IoComponent::initState();
 }
 
 void FullAdder::voltChanged()
 {
-    bool X  = m_inPin[0]->getInpState();
-    bool Y  = m_inPin[1]->getInpState();
-    bool Ci = m_inPin[2]->getInpState();
+    int Ci = m_ciPin->getInpState() ? 1 : 0;
 
-    bool sum = (X ^ Y) ^ Ci;                    // Sum
-    bool co  = (X & Ci) | (Y & Ci) | (X & Y);   // Carry out
+    int A=0, B=0;
+    for( int i=0; i<m_bits; ++i )
+    {
+        if( m_inA[i]->getInpState() ) A |= 1<<i;
+        if( m_inB[i]->getInpState() ) B |= 1<<i;
+    }
+    m_nextOutVal = A + B + Ci;
 
-    m_nextOutVal = 0;
-    if( sum ) m_nextOutVal += 1;
-    if( co  ) m_nextOutVal += 2;
     scheduleOutPuts( this );
+}
+
+void FullAdder::setBits( int b )
+{
+    if( b < 1 ) b = 1;
+    if( b == m_bits ) return;
+
+    m_outPin.resize( b+1 );
+
+    if( b > m_bits )  // Add pins
+    {
+        m_inA.resize( b );
+        m_inB.resize( b );
+        m_outS.resize( b );
+
+        for( int i=0; i<b; ++i )
+        {
+            int yB = i+b+2;
+
+            if( i < m_bits ) m_inB.at(i)->setY( yB*8 );
+            else{
+                QString iStr = QString::number(i);
+
+                QString posStr = QString::number( i+1 );
+                if( posStr.size() < 2 ) posStr.prepend("0");
+                m_inA.at(i) = createPin("IL"+posStr+"A"+iStr, m_id+"-inA"+iStr );
+
+                posStr = QString::number( yB );
+                if( posStr.size() < 2 ) posStr.prepend("0");
+                m_inB.at(i) = createPin("IL"+posStr+"B"+iStr, m_id+"-inB"+iStr );
+
+                posStr = QString::number( i+2 );
+                if( posStr.size() < 2 ) posStr.prepend("0");
+                m_outS.at(i) = createPin("OR"+posStr+"S"+iStr, m_id+"-outS"+iStr );
+                m_outPin.at(i) = m_outS.at(i);
+            }
+        }
+    }
+    else if( b < m_bits ) // Remove pins
+    {
+        int toDelete = m_bits-b;
+        deletePins( &m_inA, toDelete );
+        deletePins( &m_inB, toDelete );
+        deletePins( &m_outS, toDelete );
+
+        for( int i=0; i<b; ++i ) m_inB.at(i)->setY( (i+b+2)*8 );
+    }
+
+    QString iStr = ( b > 1 ) ? "0" : "";
+    m_inA.at(0)->setLabelText("A"+iStr);
+    m_inB.at(0)->setLabelText("B"+iStr);
+    m_outS.at(0)->setLabelText("S"+iStr);
+
+    m_outPin.at( b ) = m_coPin;
+    m_coPin->setY( b*8 + 16 );
+    m_bits = b;
+
+    m_height = m_bits*2+2;
+    if( m_bits == 1 ) m_height = 4;
+    m_area = QRect(m_area.x(), 0, m_width*8, m_height*8 );
+
+    Circuit::self()->update();
+}
+
+void FullAdder::remove()
+{
+    for( int i=0; i<m_bits; ++i )
+    {
+        m_inA[i]->removeConnector();
+        m_inB[i]->removeConnector();
+    }
+    m_ciPin->removeConnector();
+    m_coPin->removeConnector();
+
+    IoComponent::remove();
 }
--- src/components/logic/fulladder.h
+++ src/components/logic/fulladder.h
@@ -6,22 +6,39 @@
 #ifndef FULLADDER_H
 #define FULLADDER_H
 
-#include "logiccomponent.h"
+#include "iocomponent.h"
+#include "e-element.h"
 
 class LibraryItem;
 
-class FullAdder : public LogicComponent
+class FullAdder : public IoComponent, public eElement
 {
     public:
         FullAdder( QString type, QString id );
         ~FullAdder();
 
-        static Component* construct( QString type, QString id );
-        static LibraryItem* libraryItem();
+ static Component* construct( QString type, QString id );
+ static LibraryItem* libraryItem();
 
         virtual void stamp() override;
         virtual void voltChanged() override;
         virtual void runEvent() override { IoComponent::runOutputs(); }
+
+        virtual void remove() override;
+
+        int bits() { return m_bits; }
+        void setBits( int b );
+
+    private:
+
+        int m_bits;
+
+        IoPin* m_ciPin;
+        IoPin* m_coPin;
+
+        std::vector<IoPin*> m_inA;
+        std::vector<IoPin*> m_inB;
+        std::vector<IoPin*> m_outS;
 };
 
 #endif
