125297f37d63cd36dd33ba733a5feb98488a428f
Arcachofo <santigoro@mailfence.com>
2026-09-06 11:49:16 +0100

    EditorWidget: fix Ctrl+/Ctrl- zoom.

--- src/gui/editorwidget/editorwidget.h
+++ src/gui/editorwidget/editorwidget.h
@@ -60,7 +60,7 @@ class EditorWidget : public QWidget, public CompBase
        QString tabString() { return m_tab; }
 
        QFont getFont() { return m_font; }
-       void scaleFont( int s ) { m_fontSize += s;}
+       void scaleFont( int s ) { setFontSize( m_fontSize+s );}
 
        QStringList getFiles() { return m_fileList.keys(); }
        void restoreFile( QString filePath );
